aafbf11b62aa1ea181301ccfefad8d60f0015cfb
wgriffey <wgriffey15@gmail.com>
2024-09-25 10:50:29 -0600

    Added up-to-date SEO component and utils

--- src/utils/SEOJsonLoad.ts
+++ src/utils/SEOJsonLoad.ts
@@ -1,4 +1,4 @@
-import type { StructuredData, StructuredDataInput } from './structuredData';
+import type { StructuredData, StructuredDataInput } from './StructuredData';
 
 export default function jsonLoadGenerator(dataArray: StructuredDataInput[]): string {
     const graph: StructuredData[] = dataArray.map(({ type, data }) => {
