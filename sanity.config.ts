c35b154a07b1c0b04d1296f401c49b42d81e0e29
wgriffey <wgriffey15@gmail.com>
2024-09-25 13:06:05 -0600

    hopefully fixed env variables

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,13 +7,13 @@ import { loadEnv } from 'vite';
 import { schemaTypes } from './schema';
 import { resolve } from './src/utils/sanity';
 
-// const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
-// const projectId = env.PUBLIC_SANITY_PROJECT_ID;
-// const dataset = env.PUBLIC_SANITY_DATASET;
+const projectId = env.PUBLIC_SANITY_PROJECT_ID;
+const dataset = env.PUBLIC_SANITY_DATASET;
 
-const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID;
-const dataset = import.meta.env.PUBLIC_SANITY_DATASET;
+// const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID;
+// const dataset = import.meta.env.PUBLIC_SANITY_DATASET;
 
 export default defineConfig({
     name: 'default',
--- src/sanity.cli.ts
+++ src/sanity.cli.ts
@@ -2,13 +2,13 @@
 import { defineCliConfig } from 'sanity/cli';
 import { loadEnv } from 'vite';
 
-// const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 
-// const projectId = env.PUBLIC_SANITY_PROJECT_ID;
-// const dataset = env.PUBLIC_SANITY_DATASET;
+const projectId = env.PUBLIC_SANITY_PROJECT_ID;
+const dataset = env.PUBLIC_SANITY_DATASET;
 
-const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID;
-const dataset = import.meta.env.PUBLIC_SANITY_DATASET;
+// const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID;
+// const dataset = import.meta.env.PUBLIC_SANITY_DATASET;
 
 export default defineCliConfig({
     api: {
