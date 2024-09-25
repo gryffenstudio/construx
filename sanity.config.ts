7d8a63339ed7067b842f0ab0fec60349250f1a12
wgriffey <wgriffey15@gmail.com>
2024-09-25 12:25:32 -0600

    Updated configs

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,16 +7,17 @@ import { loadEnv } from 'vite';
 import { schemaTypes } from './schema';
 import { resolve } from './src/utils/sanity';
 
-const env = loadEnv(process.env.NODE_ENV!, process.cwd(), '');
+// const env = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+
+// const projectId = env.PUBLIC_SANITY_PROJECT_ID;
+// const dataset = env.PUBLIC_SANITY_DATASET;
 
 const projectId =
-    process.env.PUBLIC_SANITY_PROJECT_ID ||
-    import.meta.env.PUBLIC_SANITY_PROJECT_ID ||
-    env.PUBLIC_SANITY_PROJECT_ID;
+    process.env.PUBLIC_SANITY_PROJECT_ID ??
+    import.meta.env.PUBLIC_SANITY_PROJECT_ID
 const dataset =
-    process.env.PUBLIC_SANITY_DATASET ||
-    import.meta.env.PUBLIC_SANITY_DATASET ||
-    env.PUBLIC_SANITY_DATASET;
+    process.env.PUBLIC_SANITY_DATASET ??
+    import.meta.env.PUBLIC_SANITY_DATASET
 
 export default defineConfig({
     name: 'default',
