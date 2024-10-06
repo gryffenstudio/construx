43a7c2defdc1dacef8b492235f29652041f998e2
wgriffey <wgriffey15@gmail.com>
2024-10-06 00:14:36 -0600

    Fixed visual editing mode

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,6 +1,7 @@
 import groq from 'groq';
 import { loadQuery } from '../loader/loadQuery';
 import type { Category, Post } from './types';
+import { visualEditingEnabled, token } from './api';
 
 export async function getPost(slug: string): Promise<Post> {
     const query = groq`*[_type == "post" && slug.current == $slug][0]{
@@ -16,7 +17,12 @@ export async function getPost(slug: string): Promise<Post> {
                 slug
             },
         }`;
-    const { data } = await loadQuery<Post>({ query, params: { slug } });
+    const { data } = await loadQuery<Post>({
+        query: query,
+        params: { slug },
+        visualEditingEnabled: visualEditingEnabled,
+        token: token,
+    });
     return data;
 }
 
@@ -42,7 +48,11 @@ export async function getPosts(numOfPosts?: number): Promise<Post[]> {
             body
         }`;
 
-    const { data } = await loadQuery<Post[]>({ query });
+    const { data } = await loadQuery<Post[]>({
+        query: query,
+        visualEditingEnabled: visualEditingEnabled,
+        token: token,
+    });
     return data;
 }
 
@@ -63,19 +73,33 @@ export async function getRelatedPosts(slug: string, category: string): Promise<P
             body,
         }`;
 
-    const { data } = await loadQuery<Post[]>({ query, params: { category, slug } });
+    const { data } = await loadQuery<Post[]>({
+        query: query,
+        params: { category, slug },
+        visualEditingEnabled: visualEditingEnabled,
+        token: token,
+    });
 
     return data;
 }
 
 export async function getCategories(): Promise<Category[]> {
     const query = groq`*[_type == "category" && defined(slug.current)]`;
-    const { data } = await loadQuery<Category[]>({ query });
+    const { data } = await loadQuery<Category[]>({
+        query: query,
+        visualEditingEnabled: visualEditingEnabled,
+        token: token,
+    });
     return data;
 }
 
 export async function getCategory(slug: string): Promise<Category> {
     const query = groq`*[_type == "category" && slug.current == $slug][0]`;
-    const { data } = await loadQuery<Category>({ query, params: { slug } });
+    const { data } = await loadQuery<Category>({
+        query: query,
+        params: { slug },
+        visualEditingEnabled: visualEditingEnabled,
+        token: token,
+    });
     return data;
 }
