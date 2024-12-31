9a1e29a2dcdfceaf90737e33e6408e88119707a0
wgriffey <wgriffey15@gmail.com>
2024-12-30 21:29:39 -0700

    Undid astro version upgrades

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,6 +24,7 @@ if (!(env.PUBLIC_SANITY_PROJECT_ID && env.PUBLIC_SANITY_DATASET)) {
 export default defineConfig({
     site: 'https://construx-template.netlify.app',
     adapter: netlify(),
+    output: 'hybrid',
     devToolbar: {
         enabled: false,
     },
